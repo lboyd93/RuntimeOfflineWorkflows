7eed2f8aebc43d6de05d25af2d2e18eae49c534a
AVWORLD\laur8722 <lboyd@esri.com>
2018-10-23 15:20:30 -0700

    Adding finished pre-planned workflow.

--- PrePlanned/PrePlanned/MainWindow.xaml.cs
+++ PrePlanned/PrePlanned/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Esri.ArcGISRuntime.UI;
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +14,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -30,29 +34,100 @@ namespace PrePlanned
 
         public void Initialize()
         {
+            Map map = new Map(Basemap.CreateTopographic());
 
+            MyMapView.Map = map;
         }
-        public void OpenMMPK()
-        {
-            // Mobile map package to open directly from a package or an unpacked folder.
-            MobileMapPackage mobileMapPackage;
 
-            // Check whether the mobile map package supports direct read.
-            bool isDirectReadSupported = await MobileMapPackage.IsDirectReadSupportedAsync(pathToOutputPackage);
-            if (isDirectReadSupported)
+        private async void GeneratePreplannedMap()
+        {
+            ArcGISPortal portal = await ArcGISPortal.CreateAsync();
+
+            string itemID = "a9081e4d02dc49f68c6202f03bfe302f";
+
+            // Get a web map item using its ID.
+            PortalItem webmapItem = await PortalItem.CreateAsync(portal, itemID);
+
+            // Create a map from the web map item.
+            Map onlineMap = new Map(webmapItem);
+
+            // Create an OfflineMapTask from the map ...
+            OfflineMapTask takeMapOfflineTask = await OfflineMapTask.CreateAsync(onlineMap);
+            // ... or a web map portal item.
+            //takeMapOfflineTask = await OfflineMapTask.CreateAsync(webmapItem);
+
+            // Get a list of the available preplanned map areas.
+            IReadOnlyList<PreplannedMapArea> preplannedMapAreaList = await takeMapOfflineTask.GetPreplannedMapAreasAsync();
+
+            // Loop through all preplanned map areas.
+            foreach (PreplannedMapArea mapArea in preplannedMapAreaList)
             {
-                // If it does, create the mobile map package directly from the .mmpk file.
-                mobileMapPackage = await MobileMapPackage.OpenAsync(pathToOutputPackage);
+                // Load the preplanned map area so property values can be read.
+                await mapArea.LoadAsync();
+
+                // Get the area of interest (geometry) for this area.
+                Geometry aoi = mapArea.AreaOfInterest;
+
+                // Get the portal item for this area, read the title and thumbnail image.
+                PortalItem preplannedMapItem = mapArea.PortalItem;
+                string mapTitle = preplannedMapItem.Title;
+                RuntimeImage areaThumbnail = preplannedMapItem.Thumbnail;
+            }
+
+
+            PreplannedMapArea downloadMapArea = preplannedMapAreaList.First();
+
+            string pathToOutputPackage = @"C:\LaurenCDrive\TL\Readiness\Trainings\RT_Offline_Workflows\PrePlannedMap";
+
+            DownloadPreplannedOfflineMapJob preplannedMapJob = takeMapOfflineTask.DownloadPreplannedOfflineMap(downloadMapArea, pathToOutputPackage);
+
+            // Generate the offline map and download it.
+            DownloadPreplannedOfflineMapResult preplannedMapResult = await preplannedMapJob.GetResultAsync();
+
+            if (!preplannedMapResult.HasErrors)
+            {
+                // Job completed successfully and all content was generated.
+                Debug.WriteLine("Map " +
+                    preplannedMapResult.MobileMapPackage.Item.Title +
+                    " was saved to " +
+                    preplannedMapResult.MobileMapPackage.Path);
+
+                // Show the offline map in a MapView.
+                MyMapView.Map = preplannedMapResult.OfflineMap;
             }
             else
             {
-                // Otherwise, unpack the mobile map package file into a directory.
-                await MobileMapPackage.UnpackAsync(pathToOutputPackage, pathToUnpackedPackage);
-
-                // Create the mobile map package from the unpack directory.
-                mobileMapPackage = await MobileMapPackage.OpenAsync(pathToUnpackedPackage);
+                // Job is finished but one or more layers or tables had errors.
+                if (preplannedMapResult.LayerErrors.Count > 0)
+                {
+                    // Show layer errors.
+                    foreach (var layerError in preplannedMapResult.LayerErrors)
+                    {
+                        Debug.WriteLine("Error occurred when taking " +
+                            layerError.Key.Name +
+                            " offline. Error : " +
+                            layerError.Value.Message);
+                    }
+                }
+                if (preplannedMapResult.TableErrors.Count > 0)
+                {
+                    // Show table errors.
+                    foreach (var tableError in preplannedMapResult.TableErrors)
+                    {
+                        Debug.WriteLine("Error occurred when taking " +
+                            tableError.Key.TableName +
+                            " offline. Error : " +
+                            tableError.Value.Message);
+                    }
+                }
             }
         }
+
+        private void GeneratePreplannedAreas_Click(object sender, RoutedEventArgs e)
+        {
+            GeneratePreplannedMap();
+        }
+
 
         // Map initialization logic is contained in MapViewModel.cs
     }
